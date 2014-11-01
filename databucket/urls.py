4f9789673192ef08dde2f7bfea8c87e533ccaeee
Northshoot <laurynas.riliskis@gmail.com>
2014-11-01 13:48:20 -0700

    added listing

--- databucket/urls.py
+++ databucket/urls.py
@@ -12,9 +12,10 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'databucket.views.home', name='home'),
     url(r'^firestorm/list/(?P<amount>\w+)/$', 'databucket.views.firestorm', name="firestorm"),
-    url(r'^firestorm/add/(?P<action>\w+)/$$', 'databucket.views.firestormAdd', name="firestormAdd"),
-    url(r'^observations/(?P<fire_id>\w+)/$$', 'databucket.views.observations', name="observations"),
+    url(r'^firestorm/add/(?P<action>\w+)/$', 'databucket.views.firestormAdd', name="firestormAdd"),
+    url(r'^observations/(?P<fire_id>\w+)/$', 'databucket.views.observations', name="observations"),
     url(r'^analytics/$', 'databucket.views.analytics', name="analytics"),
     url(r'^heat/$', 'databucket.views.heat', name="heat"),
+    url(r'^latest/(?P<amount>\d+)/$', 'databucket.views.latest', name="latest"),
 
 )
