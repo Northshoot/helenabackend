b567c98a1253bfabbc2343e60e8fb7292838fa9a
Northshoot <laurynas.riliskis@gmail.com>
2014-11-03 01:18:26 -0600

    last fixes

--- databucket/views.py
+++ databucket/views.py
@@ -7,24 +7,29 @@ from django.db.models import Count, Sum, Avg
 import time
 
 def home(request):
-    n_dps = FirestormReading.objects.all()
-    print len(n_dps)
+    n_dps = FirestormReading.objects.filter(acc_Z__lt =16000).filter( acc_Z__gt=-16000).order_by('recorded')
+    if len(n_dps) > 900:
+        n_dps = n_dps[:900]
     data_points="["
     for n in n_dps:
         data = "[%d,%d,%d]," %(n.acc_X, n.acc_Y,n.acc_Z)
         data_points+= data
-    data_points+="[0,0,0]]"    
+    data_points+="[0,0,0]]"   
+    
+     
     return render_to_response('content.html',dict(data=data_points),
                               context_instance=RequestContext(request))
     
 test_data ="[[1, 6, 5], [8, 7, 9], [1, 3, 4], [4, 6, 8], [5, 7, 7], [6, 9, 6], [7, 0, 5], [2, 3, 3], [3, 9, 8], [3, 6, 5], [4, 9, 4], [2, 3, 3], [6, 9, 9], [0, 7, 0], [7, 7, 9], [7, 2, 9], [0, 6, 2], [4, 6, 7], [3, 7, 7], [0, 1, 7], [2, 8, 6], [2, 3, 7], [6, 4, 8], [3, 5, 9], [7, 9, 5], [3, 1, 7], [4, 4, 2], [3, 6, 2], [3, 1, 6], [6, 8, 5], [6, 6, 7], [4, 1, 1], [7, 2, 7], [7, 7, 0], [8, 8, 9], [9, 4, 1], [8, 3, 4], [9, 8, 9], [3, 5, 3], [0, 2, 4], [6, 0, 2], [2, 1, 3], [5, 8, 9], [2, 1, 1], [9, 7, 6], [3, 0, 2], [9, 9, 0], [3, 4, 8], [2, 6, 1], [8, 9, 2], [7, 6, 5], [6, 3, 1], [9, 3, 1], [8, 9, 3], [9, 1, 0], [3, 8, 7], [8, 0, 0], [4, 9, 7], [8, 6, 2], [4, 3, 0], [2, 3, 5], [9, 1, 4], [1, 1, 4], [6, 0, 2], [6, 1, 6], [3, 8, 8], [8, 8, 7], [5, 5, 0], [3, 9, 6], [5, 4, 3], [6, 8, 3], [0, 1, 5], [6, 7, 3], [8, 3, 2], [3, 8, 3], [2, 1, 6], [4, 6, 7], [8, 9, 9], [5, 4, 2], [6, 1, 3], [6, 9, 5], [4, 8, 2], [9, 7, 4], [5, 4, 2], [9, 6, 1], [2, 7, 3], [4, 5, 4], [6, 8, 1], [3, 4, 0], [2, 2, 6], [5, 1, 2], [9, 9, 7], [6, 9, 9], [8, 4, 3], [4, 1, 7], [6, 2, 5], [0, 4, 9], [3, 5, 9], [6, 9, 1], [1, 9, 2]]"
-def heat(request):
-    n_dps = FirestormReading.objects.all()
-    data_points="["
 
-    
+def heat(request):
+    n_dps = FirestormReading.objects.filter(acc_Z__lt =1200).filter( acc_Z__gt=-800).order_by('recorded')
+    if len(n_dps) > 900:
+        n_dps = n_dps[:900]
+    data_points="["
+ 
     for n in n_dps:
-        data = "[%d,%d,%d]," %(n.acc_X, n.acc_Y,n.acc_Z)
+        data = "[%d,%d,%d]," %(n.mag_X, n.mag_Y,n.mag_Z)
         data_points+= data
     data_points+="[0,0,0]]"    
     return render_to_response('heat_map.htm',dict(data=data_points),
@@ -32,17 +37,19 @@ def heat(request):
 def latest(request, amount):
     observationList = []
     total = ObservedDevice.objects.count()
-    if amount >0 and total <=amount:
-        observationList = ObservedDevice.objects.all()[:amount]
+    obsr = ObservedDevice.objects.all().order_by('seen')
+    amount=int(amount)
+    if amount >0 and total > amount:
+        observationList = obsr[:amount]
     else:
-        observationList = ObservedDevice.objects.all()
-    return render_to_response('observation_list.html',{'observationList':observationList, 'total':total},
+        observationList = obsr
+    return render_to_response('observation_list.html',{'observationList':observationList, 'total':total,'amount':amount},
                                   RequestContext(request)) 
 def observations(request, fire_id):
     fire = None
     try:
         fire = Firestorm.objects.get(pk=fire_id)
-        observationlist = ObservedDevice.objects.filter(observer=fire)
+        observationlist = ObservedDevice.objects.filter(observer=fire).order_by('seen')
         return render_to_response('observations.html',{'fire_id':fire_id, 'observationlist':observationlist},
                                   RequestContext(request)) 
     except Exception:
@@ -51,7 +58,7 @@ def observations(request, fire_id):
 def firestorm(request,  amount):
     fireList = []
     if amount == 'all':
-        fireList = Firestorm.objects.all()
+        fireList = Firestorm.objects.all().order_by('-seen')
         return render_to_response('firestorms.html',{'firestorms':fireList},
                                   RequestContext(request)) 
     else:
@@ -70,8 +77,8 @@ def firestorm(request,  amount):
         #observation= ObservedDevice.objects.filter(observer=fire)
         
         charts_firestorm=[]
-        data = FirestormReading.objects.filter(firestorm=fire).order_by('recorded')[:display_points]
-        print data
+        data = FirestormReading.objects.order_by('recorded').filter(firestorm=fire)[:display_points]
+        #print data
         charts_firestorm.append(lightCharts(fire, display_points,data))
         charts_firestorm.append(accelerometerCharts(fire, display_points,data))
         charts_firestorm.append(magnetometerCharts(fire, display_points,data))
@@ -94,7 +101,7 @@ def manucaftureCharts(fire, number, data):
             'observer']}
          ])
 
-    print data
+    #print data
 
     manucaftureCharts = Chart(
             datasource = ds, 
@@ -145,14 +152,14 @@ def accelerometerCharts(fire, number, data):
                        'text': 'Time'}}})
     return accelerometerCharts
 
-def lightCharts(fire, number,data):
+def lightCharts(fire, number, data):
 
     ds = DataPool(
        series=
         [{'options': {
             'source': data},
           'terms': [
-            ('recorded', lambda d: d.strftime("%H:%M:%S")),
+            'firestorm__local_mac',
             'light']}])
 
     lightCharts = Chart(
@@ -160,9 +167,9 @@ def lightCharts(fire, number,data):
             series_options = 
               [
                {'options': {
-                  'type': 'area'},
+                  'type': 'scatter'},
                 'terms':{
-                  'recorded': ['light']}}],
+                  'firestorm__local_mac': ['light']}}],
             chart_options = 
               {'title': {
                    'text': 'LUX'},
@@ -206,10 +213,11 @@ def magnetometerCharts(fire, number, data):
     return magnetometerCharts
 
 def analytics(request):
+    data = Firestorm.objects.all().order_by('seen')
     firestormpivotdata = DataPool(
        series=
         [{'options': {
-            'source': Firestorm.objects.all()},
+            'source': data},
           'terms': [
             'local_mac',
             'observations', 
@@ -234,62 +242,27 @@ def analytics(request):
            'xAxis': {
                 'title': {
                    'text': 'MAC of Firestrom'}}})
-    
-    #manufacturers
-#     ds = DataPool(
-#        series=
-#         [{'options': {
-#             'source': FirestormReading.objects.order_by('-recorded')[:20]},
-#           'terms': [
-#             ('recorded', lambda d: d.strftime("%H:%M:%S")),
-#             'firestorm',
-#             'light']}
-#          ])
-# 
-#     lightdata = Chart(
-#         datasource = ds, 
-#         series_options = 
-#           [{'options':{
-#               'type': 'line',
-#               'stacking': False},
-#             'terms':{
-#               'firestorm': [
-#                 'recorded',
-#                 'light']
-#               }}],
-#         chart_options = 
-#           {'title': {
-#                'text': 'Weather Data of Boston and Houston'},
-#            'xAxis': {
-#                 'title': {
-#                    'text': 'Firestorm'}}})
 
+    light_points = FirestormReading.objects.all().order_by('recorded')
     ds = DataPool(
-          series= [{'options': {
-            'source': FirestormReading.objects.all()[:100]},
+       series=
+        [{'options': {
+            'source': FirestormReading.objects.all()},
           'terms': [
-            'firestorm__local_mac', 
-            'light']}])
-    
+            'firestorm',
+            'light']}
+         ])
+
     lightdata = Chart(
-              datasource = ds, 
-              series_options = 
+        datasource = ds, 
+        series_options = 
           [{'options':{
-              'type': 'scatter'},
+              'type': 'dot',
+              'stacking': False},
             'terms':{
-              'firestorm__local_mac': [
+              'firestorm': [
                 'light']
-              }}],
-             chart_options =
-              {'title': {
-                   'text': 'Average sensed light by Firestorm'},
-               'xAxis': {
-                    'title': {
-                       'text': 'Firestorm'}},
-               'yAxis': {
-                    'title': {
-                       'text': 'Lux'}}}
-                           )
+              }}])
     
     return render_to_response('analytics.html',{'charts':[firestormdata, lightdata]},
                                   RequestContext(request)) 
@@ -328,9 +301,9 @@ def addSensorReading(request):
     removed try to easy debug in case of error
     '''
     sensor_data = request.POST.copy()
-    print sensor_data
+    #print sensor_data
     fire = sensor_data.pop('firestorm')[0]
-    print fire
+    #print fire
     sensor_data = dict((k,int(v)) for k,v in sensor_data.iteritems())
 
     #check if firestorm exist
